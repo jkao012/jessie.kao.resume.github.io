9ef6b49335fde1eb8ef85d960a5338174ffa49e9
JessieKaoArup <jessie.kao@arup.com>
2025-09-11 12:14:21 +0800

    testing 2

--- jessie-resume/src/view/MainPage.js
+++ jessie-resume/src/view/MainPage.js
@@ -4,13 +4,15 @@ import { Box, Button, Typography, useTheme } from "@mui/material";
 const MainPage = () => {
   const theme = useTheme();
   return (
-    <Box width="100%" justifyContent="center" display="flex">
+    <Box width="100%" minHeight="500px" justifyContent="center" display="flex">
       <Box
         display="flex"
         flexDirection="column"
         sx={{ justifyContent: "center" }}
       >
-        <Typography variant="h5">Jessie Kao Resume</Typography>
+        <Typography variant="h5" sx={{ color: theme.palette.primary.main }}>
+          Jessie Kao Resume
+        </Typography>
       </Box>
     </Box>
   );
